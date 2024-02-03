7ee6f22e2a891da0ae01a0932334a889868db2be
torchiklada <58109451+torchiklada@users.noreply.github.com>
2024-02-04 00:09:45 +0300

    Update School.java

--- 1_seminar/demo/very_first_project/src/main/java/ru/hse/java/School.java
+++ 1_seminar/demo/very_first_project/src/main/java/ru/hse/java/School.java
@@ -1,28 +1,20 @@
-package ru.hse.java;
+package ru_hse_java;
 
 import java.util.List;
 import java.util.Objects;
-import java.util.regex.Pattern;
 
 public class School {
-    private static final Pattern NAME_MASK = Pattern.compile("[А-Яа-я ]+");
-    static boolean nameIsValid(String name) {
-        return NAME_MASK.matcher(name).matches();
-    }
     // fie
     private String name;
-    private List<String> students;
+    private List<Student> students;
 
     public School(String name) {
         this(name, List.of());
     }
 
-    public School(String name, List<String> students) {
+    public School(String name, List<Student> students) {
         Objects.requireNonNull(name);
         Objects.requireNonNull(students);
-        if (students.stream().anyMatch(a -> !School.nameIsValid(a))) {
-            throw new IllegalArgumentException(students + " has not valid by " + NAME_MASK + " pattern.");
-        }
         this.name = name;
         this.students = students;
     }
@@ -36,11 +28,11 @@ public class School {
         this.name = name;
     }
 
-    public List<String> getStudents() {
+    public List<Student> getStudents() {
         return students;
     }
 
-    public void setStudents(List<String> students) {
+    public void setStudents(List<Student> students) {
         this.students = students;
     }
 
